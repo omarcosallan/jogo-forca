893cb3391be18a0accdfd69545b7650944f76a37
marcosallan <marcosallanm1to@gmail.com>
2022-09-04 12:03:56 -0300

    Ajustes no visual

--- script.js
+++ script.js
@@ -71,21 +71,21 @@ function drawGallows() {
     clearCanvas();
 
     brush.beginPath();
-    brush.moveTo(200, 290);
-    brush.lineTo(400, 290);
+    brush.moveTo(200, 250);
+    brush.lineTo(400, 250);
     brush.stroke();
 
     brush.beginPath();
-    brush.moveTo(250, 290);
+    brush.moveTo(250, 250);
     brush.lineTo(250, 70);
     brush.lineTo(380, 70);
-    brush.lineTo(380, 115);
+    brush.lineTo(380, 100);
     brush.stroke();
 }
 
 function drawHead() { 
     brush.beginPath();
-    brush.arc(380, 135, 20, 0, Math.PI * 2);
+    brush.arc(380, 120, 20, 0, Math.PI * 2);
     brush.stroke();
 }
 
@@ -105,13 +105,13 @@ function gallowsTest(attempts) {
             drawHead();
             break;
         case 2:
-            drawLine(380, 155, 380, 210);
+            drawLine(380, 140, 380, 210);
             break;
         case 3:
-            drawLine(380, 155, 350, 175);
+            drawLine(380, 140, 350, 160);
             break;
         case 4:
-            drawLine(380, 155, 410, 175);
+            drawLine(380, 140, 410, 160);
             break;
         case 5:
             drawLine(380, 210, 350, 230);
